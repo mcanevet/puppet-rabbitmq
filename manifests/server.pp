37f7903e6e6fe97fdf0bc53d9811c09ec86908ed
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-01-29 11:07:59 +0100

    Install rabbitmqadmin in rabbitmq::server

--- manifests/server.pp
+++ manifests/server.pp
@@ -171,4 +171,18 @@ class rabbitmq::server(
     }
   }
 
+  exec { 'Download rabbitmqadmin':
+    command => "curl http://${default_user}:${default_pass}@localhost:5${port}/cli/rabbitmqadmin -o /var/tmp/rabbitmqadmin",
+    creates => '/var/tmp/rabbitmqadmin',
+    require => Class['rabbitmq::service'],
+  }
+
+  file { '/usr/local/bin/rabbitmqadmin':
+    owner   => 'root',
+    group   => 'root',
+    source  => '/var/tmp/rabbitmqadmin',
+    mode    => '0755',
+    require => Exec['Download rabbitmqadmin'],
+  }
+
 }
